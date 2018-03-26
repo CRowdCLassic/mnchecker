c8c5d860716df4cd10dcdc1f830bc5314bdec92e
daja0923 <djjamol@gmail.com>
2018-03-26 15:29:57 +0200

    Cron task logging improved

--- mchecker-install.sh
+++ mchecker-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo git clone https://github.com/innovacointeam/mnchecker /root/mnchecker
 crontab -l > tempcron
-echo "*/30 * * * * cd /root/mnchecker && ./mnchecker --currency-handle=\"innova\" --currency-bin-cli=\"innova-cli\" --currency-datadir=\"/root/.innovacore\" &> mnchecker-cron.log" >> tempcron
+echo "*/30 * * * * /root/mnchecker/mnchecker --currency-handle=\"innova\" --currency-bin-cli=\"innova-cli\" --currency-datadir=\"/root/.innovacore\" > /root/mnchecker/mnchecker-cron.log 2>&1" >> tempcron
 crontab tempcron
 rm tempcron
